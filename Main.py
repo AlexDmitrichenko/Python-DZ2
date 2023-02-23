62260bba18f4b15fe5f1a212cd7bf59cf2dc2fa5
Alex <best1972@list.ru>
2023-02-23 10:22:03 +0300

    made task 10

--- Main.py
+++ Main.py
@@ -2,10 +2,18 @@
 # некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть, 
 # чтобы все монетки были повернуты вверх одной и той же стороной. Выведите минимальное 
 # количество монет, которые нужно перевернуть   Пример  5  -> 1 0 1 1 0  ->  2
+
 import random
 tails = 0
 heads = 1
+count = 0
 quantity = int(input('Введите количество монеток: '))
+
 for i in range (quantity):
     randNumber = random.randint(0, 1)
     print(randNumber, end=" ")
+    if randNumber > 0:
+        count += 1
+
+print (' Минимальное количество монет, которые нужно перевернуть:', 
+count if count < (quantity-count) else (quantity-count))
